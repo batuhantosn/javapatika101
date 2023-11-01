f26e8f7ad97024b7ff9f9ba23a5285bbbf3116dd
Batuhan <batuhan.tosun.96@hotmail.com>
2023-11-01 14:08:41 +0300

    ortalama hesaplama

---
+++ src/ortalamaHesaplama/Main.java
@@ -0,0 +1,36 @@
+package ortalamaHesaplama;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int mat, fiz, kim, tur, tar, muz;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Matematik notunuz : ");
+        mat = input.nextInt();
+
+        System.out.println("Fizik notunuz : ");
+        fiz = input.nextInt();
+
+        System.out.println("Kimya notunuz : ");
+        kim = input.nextInt();
+
+        System.out.println("Turkce notunuz : ");
+        tur = input.nextInt();
+
+        System.out.println("Tarih notunuz : ");
+        tar = input.nextInt();
+
+        System.out.println("Muzik notunuz : ");
+        muz = input.nextInt();
+
+        int toplam = mat + fiz + kim + tur + tar + muz;
+        double sonuc = toplam / 6;
+        System.out.println("Ortalamanız : " + sonuc);
+        boolean gecti = sonuc > 60;
+        System.out.println("Gecti : " + gecti);
+    }
+}
+
