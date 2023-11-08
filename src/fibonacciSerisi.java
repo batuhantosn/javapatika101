409571095e27c2112b9f32157945d13c3cc7a1f3
Batuhan <batuhan.tosun.96@hotmail.com>
2023-11-08 15:24:15 +0300

    döngüler ödevleri

---
+++ src/fibonacciSerisi.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class fibonacciSerisi {
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+
+        int nmbf = 1, nmbr = 0, nmbc, n; // nmbf = forward number / nmbr = number reverse
+
+        System.out.print("Enter step number: ");
+        n = inp.nextInt();
+
+
+        System.out.println(n + "-element Fibonacci series: ");
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(nmbr + " + " + nmbf + " : " + (nmbf + nmbr));
+
+            nmbc = nmbf;
+            nmbf = (nmbr + nmbf);
+            nmbr = nmbc;
+        }
+    }
+}
